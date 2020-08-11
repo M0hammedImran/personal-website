f1aaca20921784e943dc3594d2732d705b92bc85
Mohammed Imran <mohammedimran86992@gmail.com>
2020-08-11 22:34:16 +0530

    ✨ Google Map Working ✨
    Signed-off-by: Mohammed Imran <mohammedimran86992@gmail.com>

--- src/App.js
+++ src/App.js
@@ -1,12 +1,66 @@
 import React from 'react';
+
+import { GoogleMap, withGoogleMap, withScriptjs } from 'react-google-maps';
+
 import './App.css';
 
-function App() {
+const Map = () => {
+  return (
+    <GoogleMap
+      defaultCenter={{ lat: 12.957613, lng: 77.618746 }}
+      defaultZoom={16}
+    />
+  );
+};
+
+const WrappedMap = withScriptjs(withGoogleMap(Map));
+
+const App = () => {
+  console.log(process.env.NODE_ENV, process.env.REACT_APP_GOOGLE_MAPS);
   return (
     <div className="App">
-      <h1>Hello, I am a Web Developer.</h1>
+      <WrappedMap
+        googleMapURL={`https://maps.googleapis.com/maps/api/js?key=${process.env.REACT_APP_GOOGLE_MAPS}&v=3.exp&libraries=geometry,drawing,places`}
+        loadingElement={<div style={{ height: '100%' }} />}
+        containerElement={<div style={{ height: '100%' }} />}
+        mapElement={<div style={{ height: '100%' }} />}
+      />
     </div>
   );
-}
+};
 
 export default App;
+
+//
+// compose(
+//   withProps({
+//     googleMapURL:
+//       'https://maps.googleapis.com/maps/api/js?key=AIzaSyCTfTyzsSzVx4N5cM3K3ivhzPgyU9H8CFY&v=3.exp&libraries=geometry,drawing,places',
+//     loadingElement: <div style={{ height: `100%` }} />,
+//     containerElement: <div style={{ height: `400px` }} />,
+//     mapElement: <div style={{ height: `100%` }} />,
+//   }),
+//   withScriptjs,
+//   withGoogleMap
+// )(({ isMarkerShown, defaultZoom, defaultCenter }) => (
+//   <GoogleMap defaultZoom={defaultZoom} defaultCenter={defaultCenter}>
+//     {isMarkerShown && <Marker position={defaultCenter} />}
+//   </GoogleMap>
+// ));
+
+// 12.957651, 77.618056
+
+// // eslint-disable-next-line no-unused-vars
+// const [mapDefault, setMapDefault] = useState({
+//   center: {
+//     lat: 12.957613,
+//     lng: 77.618746,
+//   },
+//   zoom: 16,
+// });
+
+/* <MyMapComponent
+  isMarkerShown={false}
+  defaultZoom={mapDefault.zoom}
+  defaultCenter={mapDefault.center}
+/> */
