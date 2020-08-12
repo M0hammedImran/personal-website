9928c4e1506eab826f69ca04dee586c6ba330c6d
Mohammed Imran <mohammedimran86992@gmail.com>
2020-08-12 14:18:49 +0530

    😐 Google maps fu***d up, using mapbox.
    Signed-off-by: Mohammed Imran <mohammedimran86992@gmail.com>

--- src/App.js
+++ src/App.js
@@ -1,65 +1,36 @@
 import React from 'react';
 
-import { GoogleMap, withGoogleMap, withScriptjs } from 'react-google-maps';
-
 import './App.css';
 
-const Map = () => {
-  return (
-    <GoogleMap
-      defaultCenter={{ lat: 12.957613, lng: 77.618746 }}
-      defaultZoom={16}
-    />
-  );
-};
+import ReactMapboxGl, { Layer, Feature } from 'react-mapbox-gl';
 
-const WrappedMap = withScriptjs(withGoogleMap(Map));
+const mapboxStyle = 'mapbox://styles/mohammedimran/ckdr0oy7y0is919nlc28zs1kz';
+const Map = ReactMapboxGl({
+  accessToken:
+    'pk.eyJ1IjoibW9oYW1tZWRpbXJhbiIsImEiOiJja2RyMGlldTIxYThwMnd0djdpbzlpcXk5In0.4eJwVf_Sr2XZrGSejyBqAA',
+});
 
 const App = () => {
   return (
     <div className="App">
-      <WrappedMap
-        googleMapURL={`https://maps.googleapis.com/maps/api/js?key=AIzaSyCTfTyzsSzVx4N5cM3K3ivhzPgyU9H8CFY&v=3.exp&libraries=geometry,drawing,places`}
-        loadingElement={<div style={{ height: '100%' }} />}
-        containerElement={<div style={{ height: '100%' }} />}
-        mapElement={<div style={{ height: '100%' }} />}
-      />
+      <h1>Map</h1>
+      <Map
+        style={{ mapboxStyle }}
+        containerStyle={{
+          height: '100vh',
+          width: '100vw',
+        }}
+        interactive={false}
+      >
+        <Layer type="symbol" id="marker" layout={{ 'icon-image': 'marker-15' }}>
+          <Feature coordinates={[12.957651, 77.618056]} />
+        </Layer>
+      </Map>
     </div>
   );
 };
 
 export default App;
 
-//
-// compose(
-//   withProps({
-//     googleMapURL:
-//       'https://maps.googleapis.com/maps/api/js?key=AIzaSyCTfTyzsSzVx4N5cM3K3ivhzPgyU9H8CFY&v=3.exp&libraries=geometry,drawing,places',
-//     loadingElement: <div style={{ height: `100%` }} />,
-//     containerElement: <div style={{ height: `400px` }} />,
-//     mapElement: <div style={{ height: `100%` }} />,
-//   }),
-//   withScriptjs,
-//   withGoogleMap
-// )(({ isMarkerShown, defaultZoom, defaultCenter }) => (
-//   <GoogleMap defaultZoom={defaultZoom} defaultCenter={defaultCenter}>
-//     {isMarkerShown && <Marker position={defaultCenter} />}
-//   </GoogleMap>
-// ));
-
 // 12.957651, 77.618056
-
-// // eslint-disable-next-line no-unused-vars
-// const [mapDefault, setMapDefault] = useState({
-//   center: {
-//     lat: 12.957613,
-//     lng: 77.618746,
-//   },
-//   zoom: 16,
-// });
-
-/* <MyMapComponent
-  isMarkerShown={false}
-  defaultZoom={mapDefault.zoom}
-  defaultCenter={mapDefault.center}
-/> */
+// ? Mapbox Style: "mapbox://styles/mohammedimran/ckdr0oy7y0is919nlc28zs1kz"
